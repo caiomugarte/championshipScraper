42a7300112a808d424d34964ae3a0378c4b880c8
Caio Henrique Mugarte Tonhá Moreira <75679658+caiomugarte@users.noreply.github.com>
2022-09-25 00:05:29 -0300

    WIP NOTEBOOK 25/09/2022

--- server/meme.js
+++ server/meme.js
@@ -1,64 +1,48 @@
 const { default: axios } = require("axios");
 const fs = require("fs");
-var historico = "";
-var partidasId = [];
-var partidasLobby = [];
 
+var urlTeste = []
+var partidas = [];
 module.exports = {
-  getHistorico: function () {
-    const axios = require("axios");
-    const resultadosLobbyURL =
-      "https://gamersclub.com.br/players/get_playerLobbyResults/latest/1";
-    const gclubsess = "gclubsess=93b1ca845d0e943858deb03d843abf64cb651d64";
-    var data;
-    try {
-      axios
-        .get(resultadosLobbyURL, {
-          headers: {
-            cookie: gclubsess,
-          },
-        })
-        .then((response) => {
-          data = response.data;
-          setHistorico(data);
-          setPartidasId(historico);
-        })
-    } catch (error) {
-      console.log(error, error.message);
-    }
-    return historico;
+  getData: async function(req, res){
+    var historico = await getHistorico();
+    await setPartidas(historico);
+    
+    trataResponse(historico, req, res)
   },
-  
+}
 
-  getPartidasLobbyData: function() {
-    var data;
-    partidasId.forEach(function(id) {
-      const partidasLobbyDataURL = `https://gamersclub.com.br/lobby/match/${id}/1`
-      try {
-        axios.get(partidasLobbyDataURL).then((response) => {
-          data = response.data;
-          setPartidasLobby(data);
-        })
-      } catch (error) {
-        console.log(error, error.message);
-      }
-    })
-    return partidasLobby;
+function getPartidasId(historico){
+  var partidasId = []; 
+  historico.map(function (historicoObj) {
+    partidasId.push(historicoObj.idlobby_game)
+  })
+  return partidasId
+}
+async function setPartidas(historico) {
+  var partidasId = getPartidasId(historico);
+  axios.all(partidasId, 
+  console.log(urlTeste);
+}
+
+function trataResponse(historico,  req, res) {
+  res.json({historico: historico })
+}
+
+async function getHistorico() {
+  const axios = require("axios");
+  const resultadosLobbyURL =
+    "https://gamersclub.com.br/players/get_playerLobbyResults/latest/1";
+  const gclubsess = "gclubsess=93b1ca845d0e943858deb03d843abf64cb651d64";
+  try {
+    var response = await axios.get(resultadosLobbyURL, {
+      headers: {
+        cookie: gclubsess,
+      },
+    });
+    var historico = response.data.lista;
+    return historico
+  } catch (error) {
+    console.log(error, error.message)
   }
 }
-
-
-function setHistorico(data) {
-  historico = data.lista;
-}
-
-function setPartidasId(partidas) {
-  partidas.forEach(function (partida) {
-    partidasId.push(partida.idlobby_game);
-  })
-}
-
-function setPartidasLobby(data) {
-  partidasLobby = "asd";
-}
-
