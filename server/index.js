6faf1810ecb6f169790b68ac570dea4c647000b3
Caio Henrique Mugarte Tonhá Moreira <75679658+caiomugarte@users.noreply.github.com>
2022-10-27 19:56:57 -0300

    Teste Correção StackOverflow Deploy

--- server/index.js
+++ server/index.js
@@ -75,8 +75,12 @@ function fetchData() {
 
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 app.get("/liquipedia", (req, res) => {
